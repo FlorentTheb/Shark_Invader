1ca42da5bdfc8346b7024b1ec0cb1c2f5524992f
FlorentTheb <florenthebault@gmail.com>
2025-04-15 21:50:15 +0200

    Modularité des boutons dans le menu

--- menu.lua
+++ menu.lua
@@ -1,84 +1,114 @@
 local menu = {}
 
-local buttons = {}
-
 function menu.init()
     menu.font = love.graphics.newFont("__fonts__/bubbles.ttf", 100)
-    menu.fontSizeRatio = 0.75
-    menu.playButton = {
-        text = "Play",
-        speedTransi = 1500,
-        speedFading = 2,
-        isInteractable = false,
-        fadeRatio = 1,
-        color = {0, .54, 0.9, 1},
-        currentPos = {
-            x = -menu.font:getWidth("Play"),
-            y = love.graphics.getHeight() * .3
-        },
-        finalPos = {
-            x = love.graphics.getWidth() * .5 - menu.font:getWidth("Play") * .5,
-            y = love.graphics.getHeight() * .3
-        }
+    menu.fontUpperSpace = menu.font:getHeight() * 0.125
+
+    menu.buttonTexts = {}
+    menu.createButtonText("Play")
+    menu.createButtonText("Options")
+    menu.createButtonText("Exit")
+    menu.buttonPadding = 20
+    menu.buttonWidth = 0
+    menu.buttonHeight = menu.font:getHeight() + 2 * menu.buttonPadding
+    menu.buttonShadowOffset = 5
+
+    for n = 1, #menu.buttonTexts do
+        menu.buttonWidth = math.max(menu.buttonWidth, math.floor(menu.buttonTexts[n].font:getWidth(menu.buttonTexts[n].text)) + 2 * menu.buttonPadding)
+        menu.buttonTexts[n].finalePos.x = love.graphics.getWidth() * .5
+        menu.buttonTexts[n].finalePos.y = math.floor(love.graphics.getHeight() / (#menu.buttonTexts + 1)) * n
+
+        print(menu.buttonWidth)
+    end
+
+    for n = 1, #menu.buttonTexts do
+        menu.buttonTexts[n].currentPos.x = -menu.buttonWidth
+        menu.buttonTexts[n].currentPos.y = menu.buttonTexts[n].finalePos.y
+    end
+    menu.animation = {
+        buttonSpeed = 500,
+        deltaStartTimer = 2,
+        currentTime = 0
     }
 end
 
-function menu.update(dt)
-    if menu.playButton.currentPos.x < menu.playButton.finalPos.x then
-        menu.playButton.currentPos.x = menu.playButton.currentPos.x + menu.playButton.speedTransi * dt
-        if menu.playButton.speedTransi > 100 then
-            menu.playButton.speedTransi = menu.playButton.speedTransi - 1.55 * menu.playButton.speedTransi * dt
-        end
-    elseif menu.playButton.currentPos.x > menu.playButton.finalPos.x then
-        menu.playButton.currentPos.x = menu.playButton.finalPos.x
-        menu.playButton.isInteractable = true
-    else
-        menu.playButton.isInteractable = true
-    end
+function menu.createButtonText(buttonText)
+    local b = {}
+    b.font = menu.font
+    b.text = buttonText
+    b.currentPos = {x, y}
+    b.finalePos = {x, y}
+    b.origin = {
+        x = b.font:getWidth(b.text) * .5,
+        y = menu.fontUpperSpace + (b.font:getAscent() - menu.fontUpperSpace) * .5
+    }
+    b.isHover = false
 
-    if menu.isHover() then
-        menu.playButton.color = {0, .36, .6, 1}
-        if scene == "menu" then
-            love.mouse.setCursor(love.mouse.getSystemCursor("hand"))
-        end
-    else
-        menu.playButton.color = {0, .54, 0.9, 1}
-        love.mouse.setCursor(love.mouse.getSystemCursor("arrow"))
-    end
+    table.insert(menu.buttonTexts, b)
+end
+
+function menu.update(dt)
+    updateButtonsPosition(dt)
+    checkButtonHover()
 end
 
 function menu.draw()
-    --love.graphics.line(love.graphics.getWidth() * .5, 0, love.graphics.getWidth() * .5, love.graphics.getHeight())
-    love.graphics.push("all")
-    local button = menu.playButton
-    local currentColor = button.color
-    love.graphics.setColor({0, 0, 0, 0.3})
-    --love.graphics.circle("fill", menu.playButton.currentPos.x, menu.playButton.currentPos.y, 5)
-    love.graphics.rectangle("fill", menu.playButton.currentPos.x - 15, menu.playButton.currentPos.y - 5, menu.font:getWidth(menu.playButton.text) + 43, menu.font:getHeight() + 3, 20, 20)
-    love.graphics.setColor(currentColor)
-    love.graphics.rectangle("fill", menu.playButton.currentPos.x - 20, menu.playButton.currentPos.y - 10, menu.font:getWidth(menu.playButton.text) + 40, menu.font:getHeight(), 20, 20)
-    love.graphics.setColor({1, 1, 1, 1})
-    love.graphics.setLineWidth(5)
-    love.graphics.rectangle("line", menu.playButton.currentPos.x - 20, menu.playButton.currentPos.y - 10, menu.font:getWidth(menu.playButton.text) + 40, menu.font:getHeight(), 20, 20)
-    love.graphics.setColor({1, 1, 1, 1})
-    love.graphics.printf(button.text, menu.font, menu.playButton.currentPos.x, menu.playButton.currentPos.y, 700)
-    love.graphics.pop()
+    for n = 1, #menu.buttonTexts do
+        love.graphics.push("all")
+        local b = menu.buttonTexts[n]
+        love.graphics.setColor({0, 0, 0, 0.3})
+        love.graphics.rectangle("fill", b.currentPos.x - menu.buttonWidth * .5 + menu.buttonShadowOffset, b.currentPos.y - menu.buttonHeight * .5 + menu.buttonShadowOffset, menu.buttonWidth, menu.buttonHeight, 10, 10)
+        if b.isHover then
+            love.graphics.setColor({0, .7, .7})
+        else
+            love.graphics.setColor({0, .5, .5})
+        end
+        love.graphics.setLineWidth(5)
+        love.graphics.rectangle("fill", b.currentPos.x - menu.buttonWidth * .5, b.currentPos.y - menu.buttonHeight * .5, menu.buttonWidth, menu.buttonHeight, 10, 10)
+
+        love.graphics.setColor({1, 1, 1})
+        love.graphics.printf(b.text, b.font, b.currentPos.x, b.currentPos.y, menu.buttonWidth, "left", 0, 1, 1, b.origin.x, b.origin.y)
+        love.graphics.pop()
+    end
 end
 
-function menu.isHover()
-    local mX, mY = love.mouse.getPosition()
-    if menu.playButton.currentPos.x == menu.playButton.finalPos.x then
-        if mX < menu.playButton.currentPos.x + menu.font:getWidth(menu.playButton.text) and mX > menu.playButton.currentPos.x then
-            if mY < menu.playButton.currentPos.y + menu.font:getHeight() * menu.fontSizeRatio and mY > menu.playButton.currentPos.y then
-                if love.mouse.isDown(1) then
-                    scene = "game"
-                    love.mouse.setCursor(love.mouse.getSystemCursor("arrow"))
-                end
-                return true
-            end
+function updateButtonsPosition(dt)
+    for n = 1, #menu.buttonTexts do
+        local b = menu.buttonTexts[n]
+        menu.animation.currentTime = menu.animation.currentTime + dt
+        if b.currentPos.x < b.finalePos.x and menu.animation.currentTime > menu.animation.deltaStartTimer * (n - 1) then
+            b.currentPos.x = b.currentPos.x + menu.animation.buttonSpeed * dt
+        elseif b.currentPos.x > b.finalePos.x then
+            b.currentPos.x = b.finalePos.x
         end
     end
-    return false
+end
+
+function checkButtonHover()
+    local isHover = false
+    for n = 1, #menu.buttonTexts do
+        local b = menu.buttonTexts[n]
+        if b.currentPos.x == b.finalePos.x then
+            local mX, mY = love.mouse.getPosition()
+            if mX < b.currentPos.x + menu.buttonWidth * .5 and mX > b.currentPos.x - menu.buttonWidth * .5 then
+                if mY < b.currentPos.y + menu.buttonHeight * .5 and mY > b.currentPos.y - menu.buttonHeight * .5 then
+                    b.isHover = true
+                    isHover = true
+                else
+                    b.isHover = false
+                end
+            else
+                b.isHover = false
+            end
+        else
+            b.isHover = false
+        end
+    end
+    if isHover then
+        love.mouse.setCursor(love.mouse.getSystemCursor("hand"))
+    else
+        love.mouse.setCursor(love.mouse.getSystemCursor("arrow"))
+    end
 end
 
 return menu
